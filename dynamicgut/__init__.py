2bfaad7b9a87a0e3cf5010ab87ed5794a8420d6e
Mike Mundy <mundy.michael@mayo.edu>
2017-10-25 14:20:48 -0500

    Make it easy to log to a file

--- dynamicgut/__init__.py
+++ dynamicgut/__init__.py
@@ -1,3 +1,4 @@
 from .simulate import prepare, run_simulation
 from .modelutil import find_models_in_folder, set_model_id_prefix, get_exchange_metabolite_ids, \
     make_diet_from_models, make_medium, apply_medium
+from .logger import log_to_file
--- dynamicgut/logger.py
+++ dynamicgut/logger.py
@@ -4,6 +4,28 @@ logger = logging.getLogger('DynamicGut')
 handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.WARNING)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
+
+
+def log_to_file(file_name, level=logging.INFO, console=False):
+    """ Turn on logging to a file.
+
+    Parameters
+    ----------
+    file_name : str
+        Path to file for storing log output
+    level : int, optional
+        Log level value (0-50)
+    console : boolean, optional
+        When True, keep logging to the console
+    """
+
+    fh = logging.FileHandler(file_name)
+    fh.setFormatter(formatter)
+    fh.setLevel(level)
+    logger.addHandler(fh)
+    if not console:
+        ch = logger.handlers[0]
+        logger.removeHandler(ch)
